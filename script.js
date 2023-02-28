3fa3d4f062b7fcb9700c23a52c280f1fcb1c2ac1
kabircse2018 <kabircse2018@gmail.com>
2023-02-28 18:11:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// get things Element   
+console.log(document.getElementById('task-title'));
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+// Change Style
+    document.getElementById('task-title').style.background = "#000";
+    document.getElementById('task-title').style.width = "100%";
