3c31a83e14e54368f27cc2283bf2002c4382c24f
kabircse2018 <kabircse2018@gmail.com>
2023-03-07 19:51:56 +0600

    Create Element

--- script.js
+++ script.js
@@ -1,166 +1,227 @@
-// Change Style
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Change Style
     // document.getElementById('task-title').style.background = "#000";
     // document.getElementById('task-title').style.color = "#fff";
     // document.getElementById('task-title').style.padding = "10px";
     // document.getElementById('task-title').style.width = "100%";
 
-// Change Element
-    // const taskTitle = document.getElementById('task-title');
+// // Change Element
+//     // const taskTitle = document.getElementById('task-title');
     
-    // taskTitle.innerHTML = "Task List";
+//     // taskTitle.innerHTML = "Task List";
 
-// get ElementByClassName
+// // get ElementByClassName
 //    const items = document.getElementsByClassName('collection-list');
 
-// get ElementByTagName
-    // console.log(document.getElementsByTagName('li'));
+// // get ElementByTagName
+    console.log(document.getElementsByTagName('li'));
 
-// querySelector
+// // querySelector
     // console.log(document.querySelector('li')); 
-
-// querySelectorAll
-    // const q_items  = document.querySelectorAll('ul li');
-    // console.log(q_items);
-
-    // forEach loop
-    // q_items.forEach(function(item){
-    //     item.style.color = "red";
-    // });
-
-
-// get Children Element
-    // let val;
-
-    // const traversing_list = document.querySelector('ul.collection');
-
-    // val = traversing_list.children[1].children[0];
-
-    // val = traversing_list.children[0].children[0];
-
-    // val.style.color = "black";
-
-    // val.style.background = "pink";
-
-
-// get Parent Element
-    // let p_val;
-
-    // const parent_list = document.querySelector('li.collection-list');
-
-    // p_val = parent_list.parentElement;
-    
-    // console.log(p_val);
-
-// Create Element
-    // const create_li = document.createElement('li');
-
-    // Add ClassName
-    // create_li.className = 'collection-list';
-
-    // Add ID
-    // create_li.id = 'test';
-
-    // Add Attribute
-    // create_li.setAttribute('title', 'test-title');
-
-    // Add textContent
-    // create_li.textContent = "List 3";
-
-    // console.log(create_li);
-
-
-    // Append li as a Child to ul
-    // document.querySelector('ul.collection').appendChild(create_li);
-
-    // Create link
-    // const link = document.createElement('a');
-
-    // Add link Class
-    // link.className = 'btn btn-sm btn-danger';
-
-    // Add HTML
-    // link.innerHTML = '<i class="fa-solid fa-trash"></i>';
-
-    // Append Child
-    // create_li.appendChild(link);
-
-    // console.log(link);
-
-
-// Remove Element
-    // const lis = document.querySelectorAll('li');
-    // const list = document.querySelector('ul');
-    
-    // list.removeChild(lis[3]);
-
-
-
-
-
-
-// Assignmen
-
-
-// Change Style
-const taskTitle = document.getElementById('task-title');
-taskTitle.style.background = 'black';
-taskTitle.style.color = 'white';
-taskTitle.style.width = '100%';
-taskTitle.style.padding = '10px';
-
-// Change Element
-taskTitle.innerText = 'Task List';
-
-// get ElementByClassName
-const items = document.getElementsByClassName('collection-list');
-
-// get ElementByTagName
-const tagItem = document.getElementsByTagName('li');
-
-// querySelector
-const q_item = document.querySelector('li');
-
-// querySelectorAll
-console.log(document.querySelectorAll('li'));;
-
-// forEach loop
-
-
-// get Children Element
     
 
-// get Parent Element
+// // querySelectorAll
+//     // const q_items  = document.querySelectorAll('ul li');
+//     // console.log(q_items);
+
+//     // forEach loop
+//     // q_items.forEach(function(item){
+//     //     item.style.color = "red";
+//     // });
+
+
+// // get Children Element
+//     // let val;
+
+//     // const traversing_list = document.querySelector('ul.collection');
+
+//     // val = traversing_list.children[1].children[0];
+
+//     // val = traversing_list.children[0].children[0];
+
+//     // val.style.color = "black";
+
+//     // val.style.background = "pink";
+
+
+// // get Parent Element
+//     // let p_val;
+
+//     // const parent_list = document.querySelector('li.collection-list');
+
+//     // p_val = parent_list.parentElement;
+    
+//     // console.log(p_val);
+
+// // Create Element
+//     // const create_li = document.createElement('li');
+
+//     // Add ClassName
+//     // create_li.className = 'collection-list';
+
+//     // Add ID
+//     // create_li.id = 'test';
+
+//     // Add Attribute
+//     // create_li.setAttribute('title', 'test-title');
+
+//     // Add textContent
+//     // create_li.textContent = "List 3";
+
+//     // console.log(create_li);
+
+
+//     // Append li as a Child to ul
+//     // document.querySelector('ul.collection').appendChild(create_li);
+
+//     // Create link
+//     // const link = document.createElement('a');
+
+//     // Add link Class
+//     // link.className = 'btn btn-sm btn-danger';
+
+//     // Add HTML
+//     // link.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+//     // Append Child
+//     // create_li.appendChild(link);
+
+//     // console.log(link);
+
+
+// // Remove Element
+//     // const lis = document.querySelectorAll('li');
+//     // const list = document.querySelector('ul');
+    
+//     // list.removeChild(lis[3]);
+
+
+
+
+
+
+// // Assignmen
+
+
+// // Change Style
+// const taskTitle = document.getElementById('task-title');
+// taskTitle.style.background = 'black';
+// taskTitle.style.color = 'white';
+// taskTitle.style.width = '100%';
+// taskTitle.style.padding = '10px';
+
+// // Change Element
+// taskTitle.innerText = 'Task List';
+
+// // get ElementByClassName
+// const items = document.getElementsByClassName('collection-list');
+
+// // get ElementByTagName
+// const tagItem = document.getElementsByTagName('li');
+
+// // querySelector
+// const q_item = document.querySelector('li');
+
+// // querySelectorAll
+// q_item_all = document.querySelectorAll('li');
+
+// // forEach loop
+// q_item_all.forEach(function(item){
+//     item.style.color = "red";
+// });
+
+// // get Children Element
+
+// // let get_child_lists;
+
+// // const get_child_list = document.querySelector('.collection');
+
+// // get_child_lists = get_child_list.children[1].children[0];
+
+// // get_child_lists.style.color = 'black';
+
+
+// // get Parent Element
+// let val;
+
+// const get_parent_list = document.querySelector('ul.collection');
+
+// val = get_parent_list.parentElement.parentElement;
+
+// val.style.color = 'blue';
+
    
-// Create Element
-   
+// // Create Element
+//     const create_li = document.createElement('li');
+//    console.log(create_li);
 
-    // Add ClassName
+//     // Add ClassName
+//     create_li.className = 'test-class';
+
+//     // Add ID
+//     create_li.id = 'test_id';
+
+//     // Add Attribute
+//     create_li.setAttribute('title', 'test_title');
+
+//     // Add textContent 
+//     create_li.innerHTML = 'Hello';
+
+//     // Append li as a Child to ul
+//     document.querySelector('ul.collection').appendChild(create_li);
+
+//     // Create link
+//     const link = document.createElement('a');
+
+//     // Add link Class
+//     link.className = 'btn btn-sm btn-danger';
+
+//     // Add HTML
+//     link.innerHTML = '<i class="fa-solid fa-trash"></i>'
+
+//     // Append Child
+//     create_li.appendChild(link);
+
+
+// // Remove Element
   
 
-    // Add ID
-   
-
-    // Add Attribute
-  
-
-    // Add textContent
-
-    // Append li as a Child to ul
-
-
-    // Create link
-   
-
-    // Add link Class
- 
-
-    // Add HTML
-    
-
-    // Append Child
 
 
 
-// Remove Element
-  
+// // Test
+//     const btn = document.createElement('button');
+
+//     btn.className = 'btn btn-sm btn-dark';
+
+//     btn.innerText = 'Refresh';
+
+//     const button = document.querySelector('#task-btn');
+
+//     button.append(btn);
+
+//     console.log(button);
+//     console.log(btn);
 
